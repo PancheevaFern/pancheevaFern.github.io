2c85d4f95a917a00ad0b8522f08e6cc3b69b842b
PancheevaFern <codingboyusername3341@gmail.com>
2022-01-16 17:34:53 +0700

    Enter

---
+++ react-quotation/src/App.js
@@ -0,0 +1,111 @@
+import { useState, useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import QuotationTable from "./QuotationTable";
+
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+import useLocalStorage from 'react-localstorage-hook'
+
+function App() {
+  const itemRef = useRef();
+  const ppuRef = useRef();
+
+  const disRef = useRef();
+
+  const qtyRef = useRef();
+
+  // const [dataItems, setDataItems] = useState([]);
+  const [dataItems, setDataItems] = useLocalStorage("dataItems",[]);
+  
+
+
+  const dummyProductList = [
+    { id: "p001", name: 'Samsung LED TV 52"', price: 15000 },
+    { id: "p002", name: "Xiaomi Air Purifier", price: 4000 },
+    { id: "p003", name: "Brother Laserjet Printer", price: 5500 },
+    { id: "p004", name: "Bose Speaker", price: 20000 },
+  ];
+
+  const addItem = () => {
+    if (itemRef.current.value == "") {
+      alert("Item name is empty");
+      return;
+    }
+
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+
+    var itemObj = {
+      pid: pid,
+      item: product.name,
+      ppu: ppuRef.current.value,
+
+      dis: disRef.current.value,
+
+      qty: qtyRef.current.value,
+    };
+
+    dataItems.push(itemObj);
+    setDataItems([...dataItems]);   
+  };
+
+  const productChange = (e) => {
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+    ppuRef.current.value = product.price
+  }
+
+  const options = dummyProductList.map((v) => {
+    return <option value={v.id}>{v.name}</option>;
+  });
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={5} style={{ backgroundColor: "#eaeaea" }}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formItem">
+              <Form.Label>Item</Form.Label>
+              <Form.Select
+                aria-label="Default select example"
+                ref={itemRef}
+                onChange={productChange}
+              >
+                {options}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Price Per Unit"
+                ref={ppuRef}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formDiscount">
+              <Form.Label>Discount</Form.Label>
+              <Form.Control type="number" placeholder="Discount" ref={disRef} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formQauntity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control type="number" placeholder="Quantity" ref={qtyRef} />
+            </Form.Group>
+
+            <Button variant="outline-dark" onClick={addItem}>
+              Add
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <QuotationTable data={dataItems} setDataItems={setDataItems} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
+
